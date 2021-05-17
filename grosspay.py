6e94afb48b5ae62e77fc16701e6c4db244f8cc08
vinhyard <vinhyard1@gmail.com>
2021-05-17 05:31:54 -0500

    Added a input message when inputs are not integers

--- grosspay.py
+++ grosspay.py
@@ -1,12 +1,15 @@
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
 float(1.5)
-if float(hours) > 40.0:
-    rate1 = float(hours) - 40.0
-    rate4 = float(rate) * 1.5
-    rate2 = rate1 * rate4
-    rate3 = float(hours) * float(rate) + float(rate2)
-    print(rate3)
+if hours == int or rate == int:
+    if float(hours) > 40.0:
+        rate1 = float(hours) - 40.0
+        rate4 = float(rate) * 1.5
+        rate2 = rate1 * rate4
+        rate3 = float(hours) * float(rate) + float(rate2)
+        print(rate3)
+    else:
+        pay = float(hours) * float(rate)
+        print(pay)
 else:
-    pay = float(hours) * float(rate)
-    print(pay)
+    print('Please Input a Number')
