ec80342051824e5cb95bf5c38fdda5f7849b3b02
vinhyard <vinhyard1@gmail.com>
2021-05-17 05:25:57 -0500

    calculates grosspay + calculates overtime +40 hours

--- grosspay.py
+++ grosspay.py
@@ -2,8 +2,11 @@ hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
 float(1.5)
 if float(hours) > 40.0:
-    rate1 = float(hours) * (1.5 * float(rate))
-    print(rate1)
+    rate1 = float(hours) - 40.0
+    rate4 = float(rate) * 1.5
+    rate2 = rate1 * rate4
+    rate3 = float(hours) * float(rate) + float(rate2)
+    print(rate3)
 else:
     pay = float(hours) * float(rate)
     print(pay)
