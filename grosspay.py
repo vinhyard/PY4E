aec42a3c5bbd5f1cb00730826c953214558390bf
vinhyard <vinhyard1@gmail.com>
2021-05-17 05:14:41 -0500

    Update grosspay.py

--- grosspay.py
+++ grosspay.py
@@ -1,4 +1,9 @@
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
-pay = float(hours) * float(rate)
-print(pay)
+float(1.5)
+if float(hours) > 40.0:
+    rate1 = float(hours) * (1.5 * float(rate))
+    print(rate1)
+else:
+    pay = float(hours) * float(rate)
+    print(pay)
