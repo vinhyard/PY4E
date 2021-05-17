f65c46fe6d90e323bb13ea36b30d388899b584f1
vinhyard <vinhyard1@gmail.com>
2021-05-16 23:59:53 -0500

    temperature.py
    asks for celcius. program converts celcius to fahrenheit.

---
+++ temperature.py
@@ -0,0 +1,8 @@
+celcius = input('Enter the temperature in Celcius: ')
+float(celcius)
+celcius2 = 9//5
+float(celcius2)
+celcius1 = celcius * celcius2
+fahrenheit = float(celcius1) + float(32)
+float(fahrenheit)
+print(fahrenheit)
